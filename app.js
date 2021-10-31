850e6c5913054ce61498e719c8f7a0e01d11620c
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-11-01 00:23:00 +0500

    done without password validation

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-/* eslint-disable new-cap */
 const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const MongoDBStore = require('connect-mongodb-session')(session);
 const app = express();
-const http = require('http').Server(app);
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const User = require('./models/user');
-const {MongoClient, ObjectId} = require('mongodb');
-const uri1 = 'mongodb+srv://cg-mern-user:UVoso1gryGTswiPz@cluster0.mhwuh.mongodb.net/menproj01?retryWrites=true&w=majority';
+const {MongoClient} = require('mongodb');
 const uri = 'mongodb+srv://Admin:admin123@cluster0.vzs9g.mongodb.net/myDB?retryWrites=true&w=majority';
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const port = process.env.PORT || 3000;
-// const mySessions = 
-app.use(express.urlencoded({extended: true}));
+const bcrypt = require('bcryptjs');
+app.use(express.urlencoded());
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
+// connection to DB
 mongoose.connect(uri).then((result)=>{
   console.log('connected to Mongo');
 }).catch((error)=>{
   console.error('error connecting to Mongo', error);
 });
 
+// creating session
 const store = new MongoDBStore({
   uri: uri,
   collection: 'mySessions',
 });
-
 app.use(session({
   secret: 'a very secret key',
   resave: false,
@@ -35,112 +33,96 @@ app.use(session({
   store: store,
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', async (req, res) => {
-  console.log(typeof(req.session.id));
-  client.connect(async (err) => {
-    if (err) throw err;
+// routes
+app.get('/', (req, res) => {
+  client.connect((err) => {
+    if (err)
+      throw err;
     const mySessions = client.db('myDB').collection('mySessions');
-    console.log(req.session.id);
-    await mySessions.find({_id : req.session.id, 'session.isAuth': true}).toArray( (err, userData) => {
+    mySessions.findOne({ _id: req.session.id, 'session.isAuthenticated': true }, (err, userData) => {
       if (err) throw err;
-      console.log(userData);
-      if (userData.length > 0){
-        console.log(userData);
-        res.render('dashboard', {'hello' : 'You are already logged in'})}
-      else {
-        res.render('index');
-      }
+      if(userData) res.render('dashboard', {'message' : 'Welcome ' + userData.session.user.username, 'AlreadyLoggedInMessage': 'You are already logged In'});
+      else res.render('index');
     });
-  // if (store.find({_id : req.session.id, isAuth: true})){
-  //   res.render('dashboard', {'hello' : 'You are already logged in'});
-  // }
-  // else {
-  //   res.render('index');
-  // }
-  // console.log(req.session);
-  // console.log(req.session.id);
-  // res.render('index');
+  });
 });
-});
-
 app.get('/dashboard', (req, res) => {
-
-
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.get('/login', (req, res) => {
-  res.render('index');
-});
-
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  User.findOne(req.body, (err, result) => {
-    if (err) throw err;
-    if(result)
-      {
-        req.session.isAuth = true;
-        console.log(req.session.isAuth);
-        res.render('dashboard', {'hello' : 'You are logged in'});
-      }
-    else res.render('dashboard', {'hello' : 'You are not logged in'});
-    // console.log(req.session.isAuth);
-
-  });
-  
-});
-
-app.post('/', (req, res) => {
-  console.log(req.body);
-  User.findOne(req.body, (err, result) => {
-    if (err) throw err;
-    if(result)
-      {
-        req.session.isAuth = true;
-        console.log(req.session.isAuth);
-        res.render('dashboard', {'hello' : 'You are logged in'});
-      }
-    else res.render('dashboard', {'hello' : 'You are not logged in'});
-  // User.findOne(req.body, (err, result) => {
-  //   if (err) throw err;
-  //   if(result) res.render('dashboard', {'hello' : 'I am logged in'});
-  //   else res.render('dashboard', {'hello' : 'I am not logged in'});
-    // console.log(req.session.isAuth);
-    // req.session.isAuth = true;
-    // console.log(req.session.isAuth);
-  });
-  
-});
-
-app.post('/register', async (req, res) => {
-  const {username, email, password} = req.body;
-
-  let user = await User.findOne({email});
-
-  if (user) {
-    return res.redirect('/register');
-  }
-
-  try {
-    // const hashPassword = await bcrypt.hash(password);
-
-    user = new User({
-      username,
-      email,
-      password,
-    });
-    req.session.isAuth = true;
-    console.log(req.session.isAuth);
-    await user.save();
-  } catch (e) {
-    console.log(e);
-  }
   res.redirect('/');
 });
+app.get('/login', (req, res) => {
+  res.redirect('/');
+});
+app.get('/register', (req, res) => {
+  client.connect((err) => {
+    if (err)
+      throw err;
+    const mySessions = client.db('myDB').collection('mySessions');
+    mySessions.findOne({ _id: req.session.id, 'session.isAuthenticated': true }, (err, userData) => {
+      if (err) throw err;
+      if(userData) res.render('dashboard', {'message' : 'Welcome ' + userData.session.user.username, 'AlreadyLoggedInMessage': 'You are already logged In'} );
+      else res.render('register');
+    });
+  });
+});
 
-http.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// ================================================== login post ======================================================
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  User.findOne({username:req.body.username}, async (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    if (result){
+      const isValidPassword = await bcrypt.compare(req.body.password, result.password);
+      if(isValidPassword){
+        req.session.isAuthenticated = true;
+        req.session.user = req.body;
+        res.render('dashboard', {message: 'Welcome ' + req.session.user.username});
+      }
+      else{
+        res.render('index', {error: 'Incorrect Password', username: req.body.username});
+      }
+    }
+    else{
+      res.render('index', {error: "Username doesn't exist"});
+    }
+  });
+});
+
+// ================================================== logout ======================================================
+app.get('/logout', (req, res) =>
+{
+  req.session.destroy(err =>
+    {
+      if (err) throw err;
+      res.clearCookie('connect.sid');
+      res.redirect('/');
+    });
+});
+
+// ================================================= register ===================================================
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  let newUser = await User.findOne({email: req.body.email});
+  if (newUser){
+    return res.render('index', {error: 'You already have an account, sign in now!', username: newUser.username});
+  }
+  newUser = await User.findOne({username: req.body.username});
+  if (newUser){
+    return res.render('index', {error: 'You already have an account, sign in now!', username: newUser.username});
+  }
+  try{
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    newUser = new User(req.body);
+    await newUser.save();
+    req.session.isAuthenticated = true;
+    req.session.user = req.body;
+    res.render('dashboard', {message: req.session.user.username + ' is successfully registered'});
+  }catch(e){
+    if (e.message.indexOf('validation failed') !== -1) {
+      e = Object.values(e.errors).reduce((a, i)=> a+'<br>'+i);
+    }
+    return res.render('register', {error: e, username: req.body.username, email:req.body.email });
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/user.js
+++ models/user.js
@@ -1,21 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const {isEmail} = require('validator');
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is mandatory'],
+      minlength: [3, 'Username must be 3 or more characters'],
+      maxlength: [19, 'Username must be under 20 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'email already exist'],
+      lowercase: true,
+      validate: [isEmail, 'Please enter a valid email address'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be 6 or more characters'],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-// Compile model from schema
+  {
+    timestamps: true,
+    strict: false,
+  }
+);
+// TODO: change the schema and file name etc w.r.t Employee
 module.exports = mongoose.model('Employee', UserSchema );
